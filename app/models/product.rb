cb315daaa7a2f4a6b4989d17823fd90cb35aa4d4
Sergio Serrato <serrato@cal.berkeley.edu>
2015-10-08 16:14:17 -0700

    automated test for pos and patch in product model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,6 @@ class Product < ActiveRecord::Base
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
-    message: 'Bruh, this must be a URL for GIF, JPG or PNG image'
+    message: 'Bruh, this must be a URL for GIF, JPG or PNG image' 
   }
 end
